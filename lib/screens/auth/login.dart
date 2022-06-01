33b3367430b6b247b2ed6ed04f2c7d49fefdb61e
Z3N00 <78555919+Gurwindersingh85@users.noreply.github.com>
2022-05-31 20:37:45 -0400

    Update login.dart
    updated login page

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -1,8 +1,16 @@
 import 'package:coffee/constants.dart';
 import 'package:coffee/models/my_loader.dart';
 import 'package:coffee/models/user_model.dart';
+import 'package:coffee/providers/auth_provider.dart';
+import 'package:coffee/screens/auth/forgot_password_screen.dart';
+import 'package:coffee/screens/auth/sign_up.dart';
+import 'package:coffee/screens/auth/widgets/my_text_field.dart';
+import 'package:coffee/screens/home/home.dart';
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'package:provider/provider.dart';
 
-
+// created and updated by gurwinder
 class SignInScreen extends StatefulWidget {
   const SignInScreen({Key? key}) : super(key: key);
 
@@ -78,7 +86,53 @@ class _SignInScreenState extends State<SignInScreen> {
               const SizedBox(
                 height: 40,
               ),
-              
+              SizedBox(
+                width: double.infinity,
+                child: RaisedButton(
+                  onPressed: () async {
+                    setState(() {
+                      isLoading = true;
+                    });
+                    try {
+                      await Provider.of<AuthProvider>(context, listen: false)
+                          .signIn(_email!.trim(), _password!.trim());
+                      setState(() {
+                        isLoading = false;
+                      });
+                      Get.to(() => const Homepage());
+                    } catch (e) {
+                      setState(() {
+                        isLoading = false;
+                      });
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text(e.toString()),
+                      ));
+                    }
+                  },
+                  color: kPrimary,
+                  child: isLoading
+                      ? const MyLoader()
+                      : const Text('SIGN IN',
+                          style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              const Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Dont have an account? ',
+                      style: TextStyle(fontSize: 13)),
+                  InkWell(
+                      onTap: () {
+                        Get.to(() => const SignUpScreen());
+                      },
+                      child: isLoading
+                          ? const MyLoader()
+                          : const Text(
+                              'Create Account!!',
+                              style: TextStyle(color: kPrimary),
+                            ))
+                ],
               ),
             ],
           ),
