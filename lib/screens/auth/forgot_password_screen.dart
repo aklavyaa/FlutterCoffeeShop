16091055c769dee5b2e7776e839e7ea53c201924
Z3N00 <78555919+Gurwindersingh85@users.noreply.github.com>
2022-05-31 21:55:28 -0400

    updated
    updated

--- lib/screens/auth/forgot_password_screen.dart
+++ lib/screens/auth/forgot_password_screen.dart
@@ -3,6 +3,7 @@ import 'package:coffee/screens/auth/widgets/my_text_field.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+// updated
 class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({Key? key}) : super(key: key);
 
